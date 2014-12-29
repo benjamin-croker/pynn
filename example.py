3cdd3574e2c691962d0ccc55a315be15bcbe5e67
Benjamin Croker <benjamin.croker@gmail.com>
2014-12-29 23:09:21 +1000

    completed neural network in theano

--- example.py
+++ example.py
@@ -8,6 +8,7 @@ import pynn
 
 RANDOM_STATE = np.random.RandomState(42)
 
+
 def iris_demo():
     # load the iris dataset
     iris = load_iris()
@@ -16,20 +17,20 @@ def iris_demo():
 
     lb = LabelBinarizer()
     y = lb.fit_transform(y_labels)
-    
+
     # split into train and test datasets
     X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                         test_size=0.25,
                                                         random_state=RANDOM_STATE)
 
     # train the neural net
-    nn = pynn.PyNeuralNet(n_hidden=10, l=0.1)
+    nn = pynn.LogRegression(X_train.shape[1], y_train.shape[1])
     nn.fit(X_train, y_train, batch_size=20, n_epochs=2000, learning_rate=0.005)
 
     y_pred = nn.predict(X_test)
 
     print("iris accuracy: {}%".format(
-                accuracy_score(y_test.argmax(1), y_pred.argmax(1))*100))
+        accuracy_score(y_test.argmax(1), y_pred.argmax(1)) * 100))
 
 
 def digits_demo():
@@ -40,44 +41,21 @@ def digits_demo():
 
     lb = LabelBinarizer()
     y = lb.fit_transform(y_labels)
-    
+
     # split into train and test datasets
     X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                         test_size=0.25,
                                                         random_state=RANDOM_STATE)
 
     # train the neural net
-    nn = pynn.PyNeuralNet(n_hidden=100, l=0.1)
+    nn = pynn.NeuralNet(X_train.shape[1], y_train.shape[1], [20])
     nn.fit(X_train, y_train, batch_size=20, n_epochs=200, learning_rate=0.005)
 
     y_pred = nn.predict(X_test)
 
     print("digits accuracy: {}%".format(
-                accuracy_score(y_test.argmax(1), y_pred.argmax(1))*100))
+        accuracy_score(y_test.argmax(1), y_pred.argmax(1)) * 100))
 
 
-def theano_digits_demo():
-    # load the digits dataset
-    digits = load_digits()
-    X = digits['data']
-    y_labels = digits['target']
-
-    lb = LabelBinarizer()
-    y = lb.fit_transform(y_labels)
-
-    # split into train and test datasets
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=0.25,
-                                                        random_state=RANDOM_STATE)
-
-    # train the neural net
-    lr = pynn.LogRegression(X_train.shape[1], y_train.shape[1])
-    lr.fit(X_train, y_train, batch_size=20, n_epochs=200, learning_rate=0.005)
-
-    y_pred = lr.predict(X_test)
-
-    print("digits accuracy: {}%".format(
-                accuracy_score(y_test.argmax(1), y_pred.argmax(1))*100))
-
-# iris_demo()
-theano_digits_demo()
+iris_demo()
+digits_demo()
--- pynn.py
+++ pynn.py
@@ -5,23 +5,11 @@ import theano
 import theano.tensor as T
 
 
-def sigmoid(z):
-    """ computes the sigmoid function of z
-    """
-    return 1.0 / (1.0 + np.exp(-z))
-
-
-def sigmoid_grad(z):
-    """ computes the gradient of the sigmoid function at z
-    """
-    return sigmoid(z) * (1 - sigmoid(z))
-
-
 class LogRegressionLayer(object):
     """ class to perform logistic regression
     """
 
-    def __init__(self, input, input_size, output_size):
+    def __init__(self, input_matrix, input_size, output_size):
         # initialise weights and bias
         self._W = theano.shared(
             value=np.zeros(
@@ -40,8 +28,8 @@ class LogRegressionLayer(object):
         self._params = [self._W, self._b]
 
         # input and output points
-        self.input = input
-        self.output = T.nnet.softmax(T.dot(input, self._W) + self._b)
+        self.input = input_matrix
+        self.output = T.nnet.softmax(T.dot(self.input, self._W) + self._b)
 
     def updates(self, cost, learning_rate):
         """ rule for updating the weights
@@ -66,7 +54,7 @@ class HiddenLayer(object):
     """ class to perform model a single neural network layer
     """
 
-    def __init__(self, input, input_size, output_size, activation=T.tanh, random_state=None):
+    def __init__(self, input_matrix, input_size, output_size, activation=T.tanh, random_state=None):
         # initialise weights and bias
 
         # for a tanh activation function the weights should be random uniform in
@@ -107,8 +95,8 @@ class HiddenLayer(object):
 
         self._params = [self._W, self._b]
 
-        self.input = input
-        self.output = activation(T.dot(X, self._W) + self._b)
+        self.input = input_matrix
+        self.output = activation(T.dot(self.input, self._W) + self._b)
 
 
     def updates(self, cost, learning_rate):
@@ -129,8 +117,8 @@ class LogRegression(object):
 
         # initialise the log regression layer
         self._log_layer = LogRegressionLayer(self._input, input_size, output_size)
-        self._cost = self._log_layer.neg_log_likelihood(self._desired_output)
 
+        self._cost = self._log_layer.neg_log_likelihood(self._desired_output)
         self._output = self._log_layer.output
 
 
@@ -150,11 +138,13 @@ class LogRegression(object):
         # theano vector for indexing batches
         batch_index = T.lvector()
 
+        updates = self._log_layer.updates(self._cost, learning_rate)
+
         validate_batch = theano.function(
             inputs=[batch_index],
             outputs=self._log_layer.accuracy_score(self._desired_output),
             givens={
-                self._input:          X[batch_index],
+                self._input: X[batch_index],
                 self._desired_output: y[batch_index]
             }
         )
@@ -162,9 +152,9 @@ class LogRegression(object):
         train_batch = theano.function(
             inputs=[batch_index],
             outputs=self._cost,
-            updates=self._log_layer.updates(self._cost, learning_rate),
+            updates=updates,
             givens={
-                self._input:          X[batch_index],
+                self._input: X[batch_index],
                 self._desired_output: y[batch_index]
             }
         )
@@ -198,8 +188,30 @@ class LogRegression(object):
 
 
 class NeuralNet(object):
-    def __init__(self):
-        self._log_layer = None
+    def __init__(self, input_size, output_size, hidden_sizes):
+        """ Takes the input and output dimensions, and a list of ints,
+            representing the number of neurons in each hidden layer
+        """
+        # check there is at least one hidden layer
+        if len(hidden_sizes) < 1:
+            raise ValueError("hidden_sizes must be a list with at least one element")
+
+        # initialise connection points for data
+        self._input = T.matrix()
+        self._desired_output = T.matrix()
+
+        # add the first layer, connected to the input
+        self._layers = [HiddenLayer(self._input, input_size, hidden_sizes[0])]
+
+        # add any other hidden layers
+        for in_size, out_size in zip(hidden_sizes[:-1], hidden_sizes[1:]):
+            self._layers += [HiddenLayer(self._layers[-1].output, in_size, out_size)]
+
+        # add the last layer, a logistic regression classifier
+        self._layers += [LogRegressionLayer(self._layers[-1].output, hidden_sizes[-1], output_size)]
+
+        self._cost = self._layers[-1].neg_log_likelihood(self._desired_output)
+        self._output = self._layers[-1].output
 
     def fit(self, X, y, batch_size=20, n_epochs=1000, learning_rate=0.001):
         """ returns the cost
@@ -209,39 +221,34 @@ class NeuralNet(object):
         if y.ndim == 1:
             y = y[:, np.newaxis]
 
-        input_size = X.shape[1]
-        output_size = y.shape[1]
         n_samples = X.shape[0]
 
         # convert the input numpy arrays to theano shared variables
         X = theano.shared(np.asarray(X, dtype=theano.config.floatX), borrow=True)
         y = theano.shared(np.asarray(y, dtype=theano.config.floatX), borrow=True)
-        # define theano symbolic variables
-        X_theano = T.matrix('X')
-        y_theano = T.matrix('y')
+        # theano vector for indexing batches
         batch_index = T.lvector()
 
-        # define the logistic regression layer
-        self._log_layer = LogRegressionLayer(X_theano, input_size, output_size)
-        # the cost function
-        cost = self._log_layer.neg_log_likelihood(y_theano)
+        updates = [update
+                   for layer in self._layers
+                   for update in layer.updates(self._cost, learning_rate)]
 
         validate_batch = theano.function(
             inputs=[batch_index],
-            outputs=self._log_layer.accuracy_score(y_theano),
+            outputs=self._layers[-1].accuracy_score(self._desired_output),
             givens={
-                X_theano: X[batch_index],
-                y_theano: y[batch_index]
+                self._input: X[batch_index],
+                self._desired_output: y[batch_index]
             }
         )
 
         train_batch = theano.function(
             inputs=[batch_index],
-            outputs=cost,
-            updates=self._log_layer.updates(cost, learning_rate),
+            outputs=self._cost,
+            updates=updates,
             givens={
-                X_theano: X[batch_index],
-                y_theano: y[batch_index]
+                self._input: X[batch_index],
+                self._desired_output: y[batch_index]
             }
         )
 
@@ -264,147 +271,10 @@ class NeuralNet(object):
 
         predict_all = theano.function(
             inputs=[],
-            outputs=self._log_layer.output,
+            outputs=self._output,
             givens={
-                self._log_layer.input: X
+                self._input: X
             }
         )
 
         return predict_all()
-
-
-class PyNeuralNet(object):
-    """ class that contains a Neural Network
-        and functions for intialising and training it
-    """
-
-    def __init__(self, n_hidden, l=1, epsilon=0.12):
-        """ creates a neural network with one hidden layer
-                n_hidden: number of hidden units
-                l: the regularisation when training and calculating cost
-                epsilon: the std dev when generating initial random weights
-        """
-        self._n_hidden = n_hidden
-        self._l = l
-        self._epsilon = epsilon
-
-
-    def _cost(self, X, y):
-        # computes forward propagation
-
-        # number of training examples
-        m = X.shape[0]
-
-        # perform forward propagation. hX is the hypothesised output
-        # X already has the bias units added
-        hX = X
-        hX = sigmoid(np.dot(np.hstack((np.ones((m, 1)), hX)), self._theta1.T))
-        hX = sigmoid(np.dot(np.hstack((np.ones((X.shape[0], 1)), hX)), self._theta2.T))
-
-        # calculate the cost
-        J = (1.0 / m) * (-y * np.log(hX) - (1.0 - y) * np.log(1.0 - hX)).sum()
-        # calculate regularisation
-        reg = 0
-        reg += (self._theta1[:, 1:] ** 2).sum()
-        reg += (self._theta2[:, 1:] ** 2).sum()
-        reg *= self._l / (2.0 * m)
-        return J + reg
-
-
-    def _costGrad(self, X, y):
-        # number of training examples
-        m = X.shape[0]
-
-        # set gradient vectors
-        Delta1 = np.zeros(self._theta1.shape, float)
-        Delta2 = np.zeros(self._theta2.shape, float)
-
-        # store both thetaGrad1 and thetaGrad2 in a continuous block of memory,
-        # so that the whole set of theta parameters can easily be flattened for
-        # use by optimisation routines
-
-        thetaGrad = np.zeros(self._theta.shape)
-
-        thetaGrad1 = thetaGrad[:self._theta1.shape[0], :self._theta1.shape[1]]
-        thetaGrad2 = thetaGrad[:, self._theta1.shape[1]:].T
-
-        for i in range(m):
-            # propagate forward
-            a1 = np.vstack((1, X[i, :, np.newaxis]))
-
-            z2 = np.dot(self._theta1, a1)
-            a2 = np.vstack((1, sigmoid(z2)))
-
-            z3 = np.dot(self._theta2, a2)
-            a3 = sigmoid(z3)
-
-            # calculate error between calculated and actual
-            delta3 = a3 - y[i, :, np.newaxis]
-            # propagate error backward
-            delta2 = np.dot(self._theta2.T, delta3)[1:, :] * sigmoid_grad(z2)
-
-            # accumulate error
-            Delta1 += np.dot(delta2, a1.T)
-            Delta2 += np.dot(delta3, a2.T)
-
-        # calculate the gradients, with regularisation.
-        # Note that the first column of theta is removed, as it corresponds
-        # to the bias units
-        thetaGrad1[:] = (1.0 / m) * (Delta1 + self._l * np.hstack((
-            np.zeros((self._theta1.shape[0], 1)), self._theta1[:, 1:])))
-        thetaGrad2[:] = (1.0 / m) * (Delta2 + self._l * np.hstack((
-            np.zeros((self._theta2.shape[0], 1)), self._theta2[:, 1:])))
-        return thetaGrad.ravel()
-
-
-    def fit(self, X, y, batch_size=20, n_epochs=1000, learning_rate=0.001):
-        """ returns the cost
-        """
-
-        # make y a vertical array if it's not
-        if y.ndim == 1:
-            y = y[:, np.newaxis]
-
-        input_size = X.shape[1]
-        output_size = y.shape[1]
-        n_samples = X.shape[0]
-
-        # store both theta1 and theta2 in a continuous block of memory, so that
-        # the whole set of theta parameters can easily be flattened for use
-        # by optimisation routines
-        self._theta = np.zeros((self._n_hidden + 1, input_size + output_size + 1))
-
-        # define theta1 and theta2 as views into the theta block
-        self._theta1 = self._theta[:self._n_hidden, :input_size + 1]
-        self._theta2 = self._theta[:, input_size + 1:].T
-
-        # randomly initialise the weights
-        self._theta1[:] = np.random.rand(*self._theta1.shape) * 2 * self._epsilon - self._epsilon
-        self._theta2[:] = np.random.rand(*self._theta2.shape) * 2 * self._epsilon - self._epsilon
-
-        # generate randomised indices
-        epoch_split = ShuffleSplit(n_samples, n_iter=n_epochs, test_size=batch_size)
-
-        for epoch, (train_index, valid_index) in enumerate(epoch_split):
-            # split the training index into minibatches
-            for ind in [train_index[i:i + batch_size] for i in range(0, len(train_index), batch_size)]:
-                theta_grad = self._costGrad(X[ind], y[ind])
-                # update parameters
-                self._theta.ravel()[:] = self._theta.ravel()[:] - learning_rate * theta_grad
-
-            # check the model on the last batch in the epoch
-            y_pred = self.predict(X[valid_index])
-            print("epoch {} accuracy: {}%".format(
-                epoch,
-                accuracy_score(y[valid_index].argmax(1), y_pred.argmax(1)) * 100))
-
-
-    def predict(self, X):
-        # add the bias units to the input matrix
-        m = X.shape[0]
-
-        hX = X
-        hX = sigmoid(np.dot(np.hstack((np.ones((m, 1)), hX)), self._theta1.T))
-        hX = sigmoid(np.dot(np.hstack((np.ones((X.shape[0], 1)), hX)), self._theta2.T))
-
-        return hX
