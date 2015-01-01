1d8bbfd4fde5143340a569a468216156b33c4c46
Benjamin Croker <benjamin.croker@gmail.com>
2015-01-01 14:27:02 +1000

    allow training on single batch

--- example.py
+++ example.py
@@ -2,9 +2,12 @@ from sklearn.datasets import load_iris, load_digits
 from sklearn.metrics import accuracy_score
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import LabelBinarizer
+
 import numpy as np
+import logging
 
 import pynn
+
 
 RANDOM_STATE = np.random.RandomState(42)
 
@@ -24,10 +27,12 @@ def iris_demo():
                                                         random_state=RANDOM_STATE)
 
     # train the neural net
-    nn = pynn.LogRegression(X_train.shape[1], y_train.shape[1])
-    nn.fit(X_train, y_train, batch_size=20, n_epochs=2000, learning_rate=0.005)
+    print("Building logistic regression classifier to classify iris data")
+    lr = pynn.LogRegression(X_train.shape[1], y_train.shape[1], learning_rate=0.005)
+    print("Training")
+    lr.fit(X_train, y_train, batch_size=20, n_epochs=60, random_state=RANDOM_STATE)
 
-    y_pred = nn.predict(X_test)
+    y_pred = lr.predict(X_test)
 
     print("iris accuracy: {}%".format(
         accuracy_score(y_test.argmax(1), y_pred.argmax(1)) * 100))
@@ -48,10 +53,11 @@ def digits_demo():
                                                         random_state=RANDOM_STATE)
 
     # train the neural net
-    print("Building Model")
-    nn = pynn.NeuralNet(X_train.shape[1], y_train.shape[1], [20], learning_rate=0.005)
+    print("Building neural net to classify digits")
+    nn = pynn.NeuralNet(X_train.shape[1], y_train.shape[1],
+                        hidden_sizes=[20], learning_rate=0.05, random_state=RANDOM_STATE)
     print("Training")
-    nn.fit(X_train, y_train, batch_size=20, n_epochs=200)
+    nn.fit(X_train, y_train, batch_size=20, n_epochs=20, random_state=RANDOM_STATE)
 
     y_pred = nn.predict(X_test)
 
@@ -59,5 +65,6 @@ def digits_demo():
         accuracy_score(y_test.argmax(1), y_pred.argmax(1)) * 100))
 
 
-# iris_demo()
+logging.basicConfig(level=logging.DEBUG)
+iris_demo()
 digits_demo()
--- pynn.py
+++ pynn.py
@@ -3,6 +3,8 @@ from sklearn.cross_validation import ShuffleSplit
 import theano
 import theano.tensor as T
 
+import logging
+
 
 class LogRegressionLayer(object):
     """ class to perform logistic regression
@@ -50,7 +52,7 @@ class LogRegressionLayer(object):
 
 
 class HiddenLayer(object):
-    """ class to perform model a single neural network layer
+    """ class to model a single neural network layer
     """
 
     def __init__(self, input_matrix, input_size, output_size, activation=T.tanh, random_state=None):
@@ -97,7 +99,6 @@ class HiddenLayer(object):
         self.input = input_matrix
         self.output = activation(T.dot(self.input, self._W) + self._b)
 
-
     def updates(self, cost, learning_rate):
         """ rule for updating the weights
         """
@@ -109,122 +110,31 @@ class HiddenLayer(object):
 
 
 class LogRegression(object):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, learning_rate):
+        """ Creates a Logistic Regression classifier
+        """
+
         # initialise connection points for data
         self._input = T.matrix()
         self._desired_output = T.matrix()
 
-        # initialise the log regression layer
+        # add the logistic regression classifier
         self._log_layer = LogRegressionLayer(self._input, input_size, output_size)
 
+        # add cost and output connections
         self._cost = self._log_layer.neg_log_likelihood(self._desired_output)
         self._output = self._log_layer.output
-
-
-    def fit(self, X, y, batch_size=20, n_epochs=1000, learning_rate=0.001):
-        """ returns the cost
-        """
-
-        # make y a vertical array if it's not
-        if y.ndim == 1:
-            y = y[:, np.newaxis]
-
-        n_samples = X.shape[0]
-
-        # convert the input numpy arrays to theano shared variables
-        X = theano.shared(np.asarray(X, dtype=theano.config.floatX), borrow=True)
-        y = theano.shared(np.asarray(y, dtype=theano.config.floatX), borrow=True)
-        # theano vector for indexing batches
-        batch_index = T.lvector()
-
-        updates = self._log_layer.updates(self._cost, learning_rate)
-
-        validate_batch = theano.function(
-            inputs=[batch_index],
-            outputs=self._log_layer.accuracy_score(self._desired_output),
-            givens={
-                self._input: X[batch_index],
-                self._desired_output: y[batch_index]
-            }
-        )
-
-        train_batch = theano.function(
-            inputs=[batch_index],
-            outputs=self._cost,
-            updates=updates,
-            givens={
-                self._input: X[batch_index],
-                self._desired_output: y[batch_index]
-            }
-        )
-
-        epoch_split = ShuffleSplit(n_samples, n_iter=n_epochs, test_size=batch_size)
-
-        for epoch, (train_index, valid_index) in enumerate(epoch_split):
-            # split the training index into minibatches
-            for ind in [train_index[i:i + batch_size] for i in range(0, len(train_index), batch_size)]:
-                # don't train on incomplete batches
-                if len(ind) >= batch_size:
-                    train_batch(ind)
-
-            # check the model on the last batch in the epoch
-            print("epoch {} error: {}%".format(epoch, 100 * validate_batch(valid_index)))
-
-    def predict(self, X):
-
-        # convert the input numpy arrays to theano shared variables
-        X = theano.shared(np.asarray(X, dtype=theano.config.floatX), borrow=True)
-
-        predict_all = theano.function(
-            inputs=[],
-            outputs=self._output,
-            givens={
-                self._input: X
-            }
-        )
-
-        return predict_all()
-
-
-class NeuralNet(object):
-    def __init__(self, input_size, output_size, hidden_sizes, learning_rate):
-        """ Takes the input and output dimensions, and a list of ints,
-            representing the number of neurons in each hidden layer
-        """
-        # check there is at least one hidden layer
-        if len(hidden_sizes) < 1:
-            raise ValueError("hidden_sizes must be a list with at least one element")
-
-        # initialise connection points for data
-        self._input = T.matrix()
-        self._desired_output = T.matrix()
-
-        # add the first layer, connected to the input
-        self._layers = [HiddenLayer(self._input, input_size, hidden_sizes[0])]
-
-        # add any other hidden layers
-        for in_size, out_size in zip(hidden_sizes[:-1], hidden_sizes[1:]):
-            self._layers += [HiddenLayer(self._layers[-1].output, in_size, out_size)]
-
-        # add the last layer, a logistic regression classifier
-        self._layers += [LogRegressionLayer(self._layers[-1].output, hidden_sizes[-1], output_size)]
-
-        # add cost and output connections
-        self._cost = self._layers[-1].neg_log_likelihood(self._desired_output)
-        self._output = self._layers[-1].output
 
         # build the training, validation and testing functions
         # inputs for the theano functions
         X_theano = T.matrix('X_theano')
         y_theano = T.matrix('y_theano')
 
-        # update rules
-        updates = [update
-                   for layer in self._layers
-                   for update in layer.updates(self._cost, learning_rate)]
+        # update rules each layer.updates() will return a list of all the params
+        # these need to be flattened into a single list
+        updates = self._log_layer.updates(self._cost, learning_rate)
 
-
-        self.train_batch = theano.function(
+        self.partial_fit = theano.function(
             inputs=[X_theano, y_theano],
             outputs=self._cost,
             updates=updates,
@@ -236,7 +146,7 @@ class NeuralNet(object):
 
         self.validate_batch = theano.function(
             inputs=[X_theano, y_theano],
-            outputs=self._layers[-1].accuracy_score(self._desired_output),
+            outputs=self._log_layer.accuracy_score(self._desired_output),
             givens={
                 self._input: X_theano,
                 self._desired_output: y_theano
@@ -251,26 +161,125 @@ class NeuralNet(object):
             }
         )
 
+    def fit(self, X, y, batch_size, n_epochs, validation_size=0.25, random_state=None):
+        """ Performs minibatch training on the data. X and y must both be
+            two dimensional numpy arrays
 
-    def fit(self, X, y, batch_size=20, n_epochs=1000):
-        """ returns the cost
+            validation_size is the percentage of training examples to use for
+            tracking performance each epoch.
+
+            One epoch involves randomly splitting the data into a training and validation set.
+            All training data is used for training, and the error on the validation
+            set is reported each epoch.
         """
 
-        # make y a vertical array if it's not
-        if y.ndim == 1:
-            y = y[:, np.newaxis]
-
-        n_samples = X.shape[0]
-
-        epoch_split = ShuffleSplit(n_samples, n_iter=n_epochs, test_size=batch_size)
-
+        epoch_split = ShuffleSplit(X.shape[0], n_iter=n_epochs, test_size=validation_size,
+                                   random_state=random_state)
         for epoch, (train_index, valid_index) in enumerate(epoch_split):
             # split the training index into minibatches
-            for batch_index in [train_index[i:i + batch_size] for i in range(0, len(train_index), batch_size)]:
+            for batch_index in [train_index[i:i + batch_size]
+                                for i in range(0, len(train_index), batch_size)]:
                 # don't train on incomplete batches
                 if len(batch_index) >= batch_size:
-                    self.train_batch(X[batch_index], y[batch_index])
+                    self.partial_fit(X[batch_index], y[batch_index])
 
-            # check the model on the last batch in the epoch
-            print("epoch {} error: {}%".format(
-                        epoch, 100 * self.validate_batch(X[valid_index], y[valid_index])))
+            logging.debug("epoch {} error: {}%".format(
+                epoch, 100 * self.validate_batch(X[valid_index], y[valid_index])))
+
+
+class NeuralNet(object):
+    def __init__(self, input_size, output_size, hidden_sizes, learning_rate, random_state=None):
+        """ Creates a fully connected artificial neural network
+
+            hidden_sizes is a list of integers which specifies the number of units
+            in each hidden layer. Must have at least 1 element
+        """
+        # check there is at least one hidden layer
+        if len(hidden_sizes) < 1:
+            raise ValueError("hidden_sizes must be a list with at least one element")
+
+        if random_state is None:
+            random_state = np.random.RandomState()
+
+        # initialise connection points for data
+        self._input = T.matrix()
+        self._desired_output = T.matrix()
+
+        # add the first layer, connected to the input
+        self._hidden_layers = [HiddenLayer(self._input, input_size, hidden_sizes[0],
+                                           random_state=random_state)]
+
+        # add any other hidden layers
+        for in_size, out_size in zip(hidden_sizes[:-1], hidden_sizes[1:]):
+            self._hidden_layers += [HiddenLayer(self._hidden_layers[-1].output, in_size, out_size,
+                                                random_state=random_state)]
+
+        # add the last layer, a logistic regression classifier
+        self._log_layer = LogRegressionLayer(self._hidden_layers[-1].output, hidden_sizes[-1], output_size)
+
+        # add cost and output connections
+        self._cost = self._log_layer.neg_log_likelihood(self._desired_output)
+        self._output = self._log_layer.output
+
+        # build the training, validation and testing functions
+        # inputs for the theano functions
+        X_theano = T.matrix('X_theano')
+        y_theano = T.matrix('y_theano')
+
+        # update rules each layer.updates() will return a list of all the params
+        # these need to be flattened into a single list
+        updates = [update
+                   for layer in self._hidden_layers + [self._log_layer]
+                   for update in layer.updates(self._cost, learning_rate)]
+
+        self.partial_fit = theano.function(
+            inputs=[X_theano, y_theano],
+            outputs=self._cost,
+            updates=updates,
+            givens={
+                self._input: X_theano,
+                self._desired_output: y_theano
+            }
+        )
+
+        self.validate_batch = theano.function(
+            inputs=[X_theano, y_theano],
+            outputs=self._log_layer.accuracy_score(self._desired_output),
+            givens={
+                self._input: X_theano,
+                self._desired_output: y_theano
+            }
+        )
+
+        self.predict = theano.function(
+            inputs=[X_theano],
+            outputs=self._output,
+            givens={
+                self._input: X_theano
+            }
+        )
+
+    def fit(self, X, y, batch_size, n_epochs, validation_size=0.25, random_state=None):
+        """ Performs minibatch training on the data. X and y must both be
+            two dimensional numpy arrays
+
+            validation_size is the percentage of training examples to use for
+            tracking performance each epoch.
+
+            One epoch involves randomly splitting the data into a training and validation set.
+            All training data is used for training, and the error on the validation
+            set is reported each epoch.
+        """
+
+        epoch_split = ShuffleSplit(X.shape[0], n_iter=n_epochs, test_size=validation_size,
+                                   random_state=random_state)
+        for epoch, (train_index, valid_index) in enumerate(epoch_split):
+            # split the training index into minibatches
+            for batch_index in [train_index[i:i + batch_size]
+                                for i in range(0, len(train_index), batch_size)]:
+                # don't train on incomplete batches
+                if len(batch_index) >= batch_size:
+                    self.partial_fit(X[batch_index], y[batch_index])
+
+            logging.debug("epoch {} error: {}%".format(
+                epoch, 100 * self.validate_batch(X[valid_index], y[valid_index])))
