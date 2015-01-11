e9bb07236dba354b81b28c4e80fe693179e60d33
Benjamin Croker <benjamin.croker@gmail.com>
2015-01-11 15:26:42 +1000

    add convolution layer class

--- layers.py
+++ layers.py
@@ -1,6 +1,8 @@
 import numpy as np
 import theano
 import theano.tensor as T
+from theano.tensor.nnet import conv
+from theano.tensor.signal import downsample
 
 
 class LogRegressionLayer(object):
@@ -95,6 +97,88 @@ class HiddenLayer(object):
 
         self.input = input_matrix
         self.output = activation(T.dot(self.input, self._W) + self._b)
+
+    def updates(self, cost, learning_rate):
+        """ rule for updating the weights
+        """
+        grad_W = T.grad(cost=cost, wrt=self._W)
+        grad_b = T.grad(cost=cost, wrt=self._b)
+
+        return [(self._W, self._W - learning_rate * grad_W),
+                (self._b, self._b - learning_rate * grad_b)]
+
+
+class ConvolutionLayer(object):
+    """ class to model a single neural network layer
+    """
+
+    def __init__(self,
+                 input_matrix, n_input_maps, n_filters, filter_shape, pool_shape,
+                 activation=T.tanh, random_state=None):
+        # initialise weights and bias
+
+        # for a tanh activation function the weights should be random uniform in
+        # sqrt(-6./(n_in+n_hidden)) to sqrt(6./(n_in+n_hidden))
+        # for a sigmoid activation function, the weights should be 4 times this
+        # for any other activation functions, use the tanh initialisation
+
+        if random_state is None:
+            random_state = np.random.RandomState()
+
+        # total number of input and output connections
+        input_size = n_input_maps * np.prod(filter_shape)
+        output_size = n_filters * np.prod(filter_shape) / np.prod(pool_shape)
+
+        if activation == T.nnet.sigmoid:
+            self._W = theano.shared(
+                value=np.asarray(
+                    random_state.uniform(
+                        low=-4.0 * np.sqrt(6.0 / (input_size + output_size)),
+                        high=4.0 * np.sqrt(6.0 / (input_size + output_size)),
+                        size=(input_size, output_size)),
+                    dtype=theano.config.floatX),
+                name='W',
+                borrow=True)
+        else:
+            self._W = theano.shared(
+                value=np.asarray(
+                    random_state.uniform(
+                        low=-np.sqrt(6.0 / (input_size + output_size)),
+                        high=np.sqrt(6.0 / (input_size + output_size)),
+                        size=(input_size, output_size)),
+                    dtype=theano.config.floatX),
+                name='W',
+                borrow=True)
+
+        # b is 1D, there is one bias unit for each output filter
+        self._b = theano.shared(
+            value=np.zeros(
+                (n_filters,),
+                dtype=theano.config.floatX),
+            name='b',
+            borrow=True)
+
+        self._params = [self._W, self._b]
+        self.input = input_matrix
+
+        # convolve input then downsample with max pooling
+        conv_out = conv.conv2d(
+            input=input,
+            filters=self._W,
+            filter_shape=(n_filters, n_input_maps, filter_shape[0], filter_shape[1])
+        )
+
+        pooled_out = downsample.max_pool_2d(
+            input=conv_out,
+            ds=pool_shape,
+            ignore_border=True
+        )
+
+        # add the bias term. Since the bias is a vector (1D array), we first
+        # reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
+        # thus be broadcasted across mini-batches and feature map
+        # width & height
+        self.output = activation(pooled_out + self._b.dimshuffle('x', 0, 'x', 'x'))
 
     def updates(self, cost, learning_rate):
         """ rule for updating the weights
